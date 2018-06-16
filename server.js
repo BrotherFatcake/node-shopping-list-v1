0d806729d3777392806ce1c1ca9680422c701c05
BrotherFatcake <BrotherFatcake@brownscastle.com>
2018-06-16 16:04:14 -0500

    added get recipes

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const bodyParser = require('body-parser');
 // interact with in our GET endpoint
 const {ShoppingList} = require('./models');
 
+//tb code
+const {Recipes} = require('./models');
+
 const jsonParser = bodyParser.json();
 const app = express();
 
@@ -30,6 +33,20 @@ ShoppingList.create('peppers', 4);
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
 });
+
+
+
+//tb code
+//These are items to be loaded when ever the server starts so that there is data to return immediately
+Recipes.create('mac & cheese', ['Mac', 'Cheddar Cheese']);
+Recipes.create('Beef Fillet', ['Beef fillet', 'salt', 'pepper', 'steak sauce is an abomination!']);
+
+//tb code
+//get request to Recipes
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
+});
+
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
